453ffa3d8b7938c8365462a5456141ca33936398
Urban Knuples <urbikn@gmail.com>
2023-07-21 10:55:30 +0200

    Create file to extract embeddings from models

---
+++ src/embeddings.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+import pandas as pd
+import numpy as np
+
+def load_BiLSTM(filepath):
+    pass
+
+def load_sentenceBERT(filepath):
+    pass
+
+def load_dataset(dataset):
+    pass
+
+def extract_embeddings(model, data):
+    pass
+
+def save_embeddings(model, data):
+    pass
