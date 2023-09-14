b5d782a0d2077dd998bbf567af80fa22a3d8eb60
Urban Knuples <urbikn@gmail.com>
2023-09-14 09:02:27 +0200

    Analyize results from PCA probing

--- all_probing.sh
+++ all_probing.sh
@@ -16,15 +16,15 @@ fi
 batch_size=32
 
 # Define an array of tasks
-# tasks=("bigram_shift" "obj_number" "sentence_length" "top_constituents" "word_content" "coordination_inversion" "odd_man_out" "subj_number" "tree_depth")
-tasks=("past_present")
+tasks=("bigram_shift" "obj_number" "sentence_length" "top_constituents" "word_content" "coordination_inversion" "odd_man_out" "subj_number" "tree_depth" "past_present")
 
 # Define an array of layers
 layers=(0 1 2 3 4 5 6 7 8 9 10 11 12)
+layers=(12)
 
 # Define an array of models
 models=("mpnet-base-v2" "MiniLM-L12-v2")
-counter_limits=(6 1)
+counter_limits=(12 12)
 
 
 # Iterate over all models 
@@ -38,29 +38,34 @@ for i in "${!models[@]}"; do
     echo "=========================="
     echo ""
 
+    counter=0
     # Iterate over all tasks
     for task in "${tasks[@]}"; do
-        counter=0
-
         echo "= Task $task"
 
         # Iterate over all layers
         for layer in "${layers[@]}"; do
-	    echo "Layer: $layer"
-            python3 src/main_probing.py --model $model --task $task --seed $seed --batch_size $batch_size --training_data .embeddings/$task.txt --embedding_data .embeddings/$model-layer-$layer.$task.pt --training_id $model-layer-$layer-$seed &
+            echo "Layer: $layer"
+            python3 src/main_probing.py --model $model --task $task --seed $seed --batch_size $batch_size --training_data .embeddings/$task.txt --embedding_data .embeddings/$model-layer-$layer.$task.pca.pt --training_id $model-layer-$layer-pca-$seed &
             counter=$((counter+1))
             if [ $counter -eq $counter_limit ]; then
                 wait  # Wait for all $counter_limit scripts to complete
                 counter=0
             fi
         done
-        wait  # Wait for any remaining scripts to complete
+
+        if [ $counter -eq $counter_limit ]; then
+            wait  # Wait for all $counter_limit scripts to complete
+            counter=0
+        fi
     done
+
+    wait
 done
 
 # Now run the same experiments but for only the baseline model
 model="bilstm"
-counter_limit=6
+counter_limit=12
 
 echo ""
 echo "=========================="
@@ -68,12 +73,12 @@ echo "Starting $model Probing Tasks"
 echo "=========================="
 echo ""
 
+counter=0
 # Iterate over all tasks
 for task in "${tasks[@]}"; do
-    counter=0
 
     echo "= Task $task"
-    python3 src/main_probing.py --model $model --task $task --seed $seed --batch_size $batch_size --training_data .embeddings/$task.txt --embedding_data .embeddings/$model.$task.pt  --training_id $model-$seed &
+    python3 src/main_probing.py --model $model --task $task --seed $seed --batch_size $batch_size --training_data .embeddings/$task.txt --embedding_data .embeddings/$model.$task.pca.pt  --training_id $model-pca-$seed &
     counter=$((counter+1))
     if [ $counter -eq $counter_limit ]; then
         wait  # Wait for all $counter_limit scripts to complete
